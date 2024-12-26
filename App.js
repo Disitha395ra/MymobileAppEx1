0f25dcdbddd3a58ad0026a9f2c8366ac7be14d53
Disitha <disithar@gmail.com>
2024-12-26 14:14:48 +0530

    import and wrap app.js using safearea

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, ScrollView } from "react-native";
 import { PaperProvider } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
 import Home from "./components/Home";
 
 export default function App() {
   return (
     <PaperProvider>
-      <View style={styles.container}>
-        <Home/>
-        <StatusBar style="auto" />
-      </View>
+      <SafeAreaView>
+        <ScrollView>
+          <View style={styles.container}>
+            <StatusBar style="auto" />
+            <Home />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
     </PaperProvider>
   );
 }
@@ -20,14 +25,5 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     alignItems: "center",
     justifyContent: "center",
-  },
-  body:{
-    textAlign:"center",
-    color:"red",
-  },
-  divider:{
-    backgroundColor:"red",
-    height:2,
-    width:"50%",
   }
 });
