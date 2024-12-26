94e12d0bd24e7c951d544b0c30f59b7ee502cd8b
Disitha <disithar@gmail.com>
2024-12-26 13:59:31 +0530

    add Home.js to main App.js

--- App.js
+++ App.js
@@ -1,27 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
-import { PaperProvider, Text, Divider } from "react-native-paper";
+import { PaperProvider } from "react-native-paper";
+import Home from "./components/Home";
 
 export default function App() {
   return (
     <PaperProvider>
       <View style={styles.container}>
-        <Text variant="headlineLarge">Headline Large</Text>
-        <Text>Mango</Text>
-        <Divider style={styles.divider} />
-        <Text>Lemon</Text>
-        <Divider style={styles.divider} />
-        <Text>Mango</Text>
-        <Divider style={styles.divider} />
-        <Text variant="bodyLarge" style={styles.body}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum
-        </Text>
+        <Home/>
         <StatusBar style="auto" />
       </View>
     </PaperProvider>
---
+++ components/Home.js
@@ -0,0 +1,38 @@
+import { StyleSheet, View } from "react-native";
+import { PaperProvider, Text, Divider } from "react-native-paper";
+export default function Home() {
+  return (
+    <View>
+      <PaperProvider>
+        <Text variant="headlineLarge">My Head</Text>
+        <Text>Mango</Text>
+        <Divider style={styles.divider} />
+        <Text>Lemon</Text>
+        <Divider style={styles.divider} />
+        <Text>Mango</Text>
+        <Divider style={styles.divider} />
+        <Text variant="bodyLarge" style={styles.body}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum
+        </Text>
+      </PaperProvider>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  body: {
+    textAlign: "center",
+    color: "red",
+  },
+  divider: {
+    backgroundColor: "red",
+    height: 2,
+    width: "50%",
+  },
+});
