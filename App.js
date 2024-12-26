47890b39ae5f73c03b29ac10da71df52752bfed5
Disitha <disithar@gmail.com>
2024-12-26 12:08:55 +0530

    add text components with different varients

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { PaperProvider } from "react-native-paper";
+import { StyleSheet, View } from "react-native";
+import { PaperProvider, Text } from "react-native-paper";
 
 export default function App() {
   return (
     <PaperProvider>
       <View style={styles.container}>
-        <Text>Here is My First App</Text>
+        <Text variant="headlineLarge">Headline Large</Text>
+        <Text variant="headlineLarge">Headline Large</Text>
+        <Text variant="bodyLarge">Body Large</Text>
+        <Text variant="labelSmall">Label Small</Text>
         <StatusBar style="auto" />
       </View>
     </PaperProvider>
