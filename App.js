56fb37b291ce026fbb012cf0074e47202958fcb3
Disitha <disithar@gmail.com>
2024-12-26 13:32:56 +0530

    add divider componenet

--- App.js
+++ App.js
@@ -1,15 +1,27 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
-import { PaperProvider, Text } from "react-native-paper";
+import { PaperProvider, Text, Divider } from "react-native-paper";
 
 export default function App() {
   return (
     <PaperProvider>
       <View style={styles.container}>
         <Text variant="headlineLarge">Headline Large</Text>
-        <Text variant="headlineLarge">Headline Large</Text>
-        <Text variant="bodyLarge">Body Large</Text>
-        <Text variant="labelSmall">Label Small</Text>
+        <Text>Mango</Text>
+        <Divider style={styles.divider} />
+        <Text>Lemon</Text>
+        <Divider style={styles.divider} />
+        <Text>Mango</Text>
+        <Divider style={styles.divider} />
+        <Text variant="bodyLarge" style={styles.body}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum
+        </Text>
         <StatusBar style="auto" />
       </View>
     </PaperProvider>
@@ -23,4 +35,13 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  body:{
+    textAlign:"center",
+    color:"red",
+  },
+  divider:{
+    backgroundColor:"red",
+    height:2,
+    width:"50%",
+  }
 });
